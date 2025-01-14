3aea503a7c1d77ce47291baab64e12294be93046
RiyaGarhwal <riyagarhwal0612@gmail.com>
2025-01-14 18:23:59 +0530

    14-01-2024

--- src/app/user/user-list.component.ts
+++ src/app/user/user-list.component.ts
@@ -22,12 +22,45 @@ export class UserListComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.preload();
     this.search();
+  }
+
+  next() {
+    this.form.pageNo++;
+    console.log('pageNo => ', this.form.pageNo)
+    this.search();
+  }
+
+  previous() {
+    this.form.pageNo--;
+    console.log('pageNo => ', this.form.pageNo)
+    this.search();
+  }
+
+  preload() {
+    this.httpClient.get('http://localhost:8080/User/preload').subscribe((res: any) => {
+      this.form.preload = res.result.roleList;
+    });
   }
 
   search() {
     this.httpClient.post('http://localhost:8080/User/search/' + this.form.pageNo, this.form.searchParams).subscribe((res: any) => {
       this.form.list = res.result.data;
     })
+  }
+
+  onCheckboxChange(userId: number) {
+    console.log('Checkbox with ID', userId, 'is checked/unchecked');
+    this.form.deleteParams.id = userId;
+  }
+
+  delete() {
+    this.httpClient.get('http://localhost:8080/User/delete/' + this.form.deleteParams.id).subscribe((res: any) => {
+      this.form.message = res.result.message;
+      console.log('message => ', this.form.message)
+      this.form.pageNo = 0;
+      this.search();
+    });
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+
+  form: any = {
+    data: {},
+    inputerror: {},
+    message: '',
+    preload: []
+  }
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.preload();
+  }
+
+
+  preload() {
+    this.httpClient.get('http://localhost:8080/User/preload').subscribe((res: any) => {
+      console.log(res)
+      this.form.preload = res.result.roleList;
+    });
+  }
+
+  save() {
+    this.httpClient.post('http://localhost:8080/User/save', this.form.data).subscribe((res: any) => {
+      console.log('res => ', res)
+
+      this.form.message = '';
+      this.form.inputerror = {};
+
+      if (res.result.message) {
+        this.form.message = res.result.message;
+      }
+
+      if (!res.success) {
+        this.form.inputerror = res.result.inputerror;
+      }
+    });
+  }
 
 }
